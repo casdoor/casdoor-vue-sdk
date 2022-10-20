28a2891a150aa2e1e2371dd7c13ccb4dfedc7494
Rick Lin <74520253+DevRickLin@users.noreply.github.com>
2022-10-20 23:01:14 +0800

    fix: import the type InjectionKey from vue3 in jsdoc (#19)

--- src/token.js
+++ src/token.js
@@ -1,10 +1,9 @@
-import { InjectionKey } from "vue";
 import Sdk from "casdoor-js-sdk";
 
 const CASDOOR_SDK_TOKEN = '$casdoor-sdk';
 
 /**
  * @summary  Injection token used to `provide` / `inject` the `Sdk` instance.
- * @type { InjectionKey<Sdk> }
+ * @type { import('vue').InjectionKey<Sdk> }
  */
 export const CASDOOR_SDK_INJECTION_KEY = Symbol.for(CASDOOR_SDK_TOKEN);
